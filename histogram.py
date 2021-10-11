c47e32d6c0fde134a412794d48d97bd3af9af3d1
Mariano Shaar <a00825287@itesm.mx>
2021-10-11 15:53:02 -0500

    Codigo para graficar el histograma

---
+++ histogram.py
@@ -0,0 +1,13 @@
+from numpy import loadtxt
+import matplotlib.pyplot as plt
+
+# indice de tempo
+MY_VARIABLE_INDEX = 4
+
+# load array
+data = loadtxt('data.csv', delimiter=',')
+my_variable_data = data[MY_VARIABLE_INDEX]
+
+plt.hist(my_variable_data, 10)
+
+plt.show()
